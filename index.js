8c5f8c48ef29ac9ed2948ef3c6f889e371370b38
SoftDevCLF <cintya.lara.f@gmail.com>
2025-09-11 04:42:10 -0600

    🔃Fix format

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const characters = [
 // broken test data for exercise 6
 
 // 1. Print out the names of each character in the console, then render them in the HTML list with id "names-list"
-const namesList = document.getElementById("names-list")
+const namesList = document.getElementById("names-list");
 //callbackFn
 const createUserName = (user) => {
   console.log(user.name);
@@ -38,13 +38,12 @@ const createUserName = (user) => {
 namesList.innerHTML = users.map(createUserName).join("");
 
 /*
-*References: 
-*https://github.com/ashx3s/js-refresh-b
-*https://www.geeksforgeeks.org/javascript/how-to-creating-html-list-from-javascript-array
-*https://www.w3schools.com/js/js_array_iteration.asp#mark_map
-*https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
-*/
-
+ *References:
+ *https://github.com/ashx3s/js-refresh-b
+ *https://www.geeksforgeeks.org/javascript/how-to-creating-html-list-from-javascript-array
+ *https://www.w3schools.com/js/js_array_iteration.asp#mark_map
+ *https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+ */
 
 // 2. Print out the names of characters whose age is less than 40 in the console, then render them in the HTML list with id "young-characters-list"
 
@@ -54,43 +53,41 @@ let youngCharactersList = document.getElementById("young-characters-list");
 console.log("Names of characters whose age is less than 40:");
 
 //Callback Function for filter
-const checkAge = user => user.age < 40;
+const checkAge = (user) => user.age < 40;
 
 //Filter list and then mapping user =>
-const lessThanFortyNames= users.filter(checkAge).map(createUserName).join("");
+const lessThanFortyNames = users.filter(checkAge).map(createUserName).join("");
 
 youngCharactersList.innerHTML = lessThanFortyNames;
 
 /*
-*References:
-*https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
-*https://www.w3schools.com/jsref/jsref_filter.asp
-*/
+ *References:
+ *https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+ *https://www.w3schools.com/jsref/jsref_filter.asp
+ */
 
 // 3. Create a reusable function that takes any array and uses logic to render a list of character names in the HTML. Use this function to populate the list with id "function-list"
 
 /**
  * Function that takes any array and uses logic to render a list of character names in the HTML
- * @param {*} array 
+ * @param {*} array
  * @returns  list of names
  */
-function renderNamesList(array){
-  const itemWithNoNameList = array.filter(user => !user.name);
-  const itemWithNameList = array.filter(user => user.name);
+function renderNamesList(array) {
+  const itemWithNoNameList = array.filter((user) => !user.name);
+  const itemWithNameList = array.filter((user) => user.name);
 
-  if (itemWithNoNameList.length > 0){
-    itemWithNoNameList.map((user)=> {
+  if (itemWithNoNameList.length > 0) {
+    itemWithNoNameList.map((user) => {
       const renderError = document.getElementById("broken-array-errors");
       let message = ` <p>Error: The object with id ${user.id} is missing the name property. </p>`;
       renderError.innerHTML += message;
     });
   }
 
-  const userName = user => `<li>${user.name}</li>`;
-  return itemWithNameList.map(userName).join(""); 
-  
-};
-  
+  const userName = (user) => `<li>${user.name}</li>`;
+  return itemWithNameList.map(userName).join("");
+}
 
 //Render in HTML
 const characterNamesList = document.getElementById("function-list");
@@ -100,53 +97,52 @@ characterNamesList.innerHTML = renderNamesList(users);
 characterNamesList.innerHTML += renderNamesList(characters);
 
 /*
-*References:
-*https://www.geeksforgeeks.org/javascript/how-to-add-html-elements-dynamically-using-javascript/#approach-1-using-innerhtml
-*/
+ *References:
+ *https://www.geeksforgeeks.org/javascript/how-to-add-html-elements-dynamically-using-javascript/#approach-1-using-innerhtml
+ */
 
 // 4. Create a function that takes an array and an age threshold parameter. The function should only display characters whose age is below the given number. Render results in the list with id "age-filter-list"
 
-
 /**
  * Function that display characters whose age is below the given number
- * @param {*} array 
- * @param {*} age 
+ * @param {*} array
+ * @param {*} age
  * @returns  list of filtered names
  */
 
-function ageFilter(array, age){
-  const itemWithNoName = array.filter(user => !user.name);
-  if (itemWithNoName.length > 0){
-    itemWithNoName.map((user)=> {
+function ageFilter(array, age) {
+  const itemWithNoName = array.filter((user) => !user.name);
+  if (itemWithNoName.length > 0) {
+    itemWithNoName.map((user) => {
       const renderError = document.getElementById("broken-array-errors");
       let message = ` <p>Error: The object with id ${user.id} with age ${user.age} is missing the name property. </p>`;
       renderError.innerHTML += message;
     });
   }
-  const filterAgeAndNameValidation = array.filter(user => age > user.age && user.name);
-  if (filterAgeAndNameValidation.length === 0){
-    return "<li>No characters found below the specified age.</li>";
+  const filterAgeAndNameValidation = array.filter(
+    (user) => age > user.age && user.name
+  );
+  if (filterAgeAndNameValidation.length === 0) {
+    return `<li>No characters in the list  were found below the specified age.</li>`;
   }
-  return filterAgeAndNameValidation.map(user => `<li>${user.name}</li>`).join("");
-  
-};
+  return filterAgeAndNameValidation
+    .map((user) => `<li>${user.name}</li>`)
+    .join("");
+}
 
 //Render in html
 
 const renderAgeFilteredList = document.getElementById("age-filter-list");
 
-renderAgeFilteredList.innerHTML = ageFilter(users,25);
-renderAgeFilteredList.innerHTML = ageFilter(characters,25);
+renderAgeFilteredList.innerHTML =
+  ageFilter(users, 25) + ageFilter(characters, 25);
 
 /*
-*References:
-*https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
-*https://www.w3schools.com/js/js_if_else.asp
-*/
-
+ *References:
+ *https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
+ *https://www.w3schools.com/js/js_if_else.asp
+ */
 
 // 5. Add error handling to your functions that will log an error message using console.error() if any object doesn't have a "name" property. Display any error messages in the div with id "error-messages"
 
 // 6. Test your error handling by creating a second array that's intentionally broken (missing name properties) and passing it to your functions. Verify that your error handling works correctly and displays errors in the div with id "broken-array-errors"
-
-
